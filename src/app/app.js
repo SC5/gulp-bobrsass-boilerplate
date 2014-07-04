e59c870d62a0d5f2832bdc6d2dcb770329ad2adf
Lauri Svan <lauri.svan@sc5.io>
2014-07-04 11:40:29 +0300

    Update dependencies; Add strict JSHint settings & fix the sources accordingly

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,8 @@
-var app = require('./app.js'),
-    $ = require('jquery');
+var $ = require('jquery');
 
 function start() {
+  // Just get ri
+
   // Start the app here
   $('#status').html('If you can read this text, your stack should be alright.');
 }
