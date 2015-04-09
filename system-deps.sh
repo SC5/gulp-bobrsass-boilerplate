0b1cac466d7334e0fd9b703982661dba29aea6fa
Petri Mahanen <petri@sc5.io>
2015-04-09 18:43:49 +0300

    Cleaner messaging. Using  instead of .

--- system-deps.sh
+++ system-deps.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-if ruby --version; then
+echo 'Checking Ruby:'
+
+if ruby --version 2>/dev/null; then
     echo 'Ruby found.'
+    echo
 else
     echo 'DEPENDENCY MISSING!'
     echo 'Install Ruby <https://www.ruby-lang.org/>'
-    return 1;
+    exit 1
 fi
 
-if bundle --version; then
+echo 'Checking Bundler:'
+
+if bundle --version 2>/dev/null; then
     echo 'Bundler found, running `bundle install`.'
     echo 'If this fails, try to update rubygems: `gem update --system`.'
+    echo
     bundle install
 else
     echo 'DEPENDENCY MISSING!'
     echo 'Install <http://bundler.io/>, usually `gem install bundler`.'
-    return 1;
+    exit 2
 fi
